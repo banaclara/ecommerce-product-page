34056f5a49bd8dce916cfc95e073564ae7bcb692
Ana <anaclaraqb@gmail.com>
2023-11-20 21:45:16 -0300

    update border and sizes

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ function changeImage(thumbnail) {
 
   thumbnails.forEach(function (otherThumbnail) {
     otherThumbnail.classList.remove("selectedthumbnail");
-    otherThumbnail.style.opacity = "1";
   });
 
   thumbnail.classList.add("selectedthumbnail");
 
   principalImage.src = thumbnail.getAttribute("data-src");
+}
+
+function border(thumbnail) {
+  var thumbBorder = document.querySelectorAll(".thmb");
+
+  thumbBorder.forEach(function (container) {
+    container.classList.remove("thmborder");
+  });
+
+  thumbnail.classList.add("thmborder");
 }
